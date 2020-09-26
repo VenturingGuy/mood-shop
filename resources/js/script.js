2c7a2e662c9040581bc3e6374288305b9ff9b930
Luis <LVentura335@Gmail.com>
2020-09-26 07:39:26 -0700

    add button connected to cart

--- resources/js/script.js
+++ resources/js/script.js
@@ -38,89 +38,96 @@ for (let i=0; i<data.length; ++i) {
     newDiv.appendChild(button)
     
     itemsContainer.appendChild(newDiv)
-
-    const cart = []
-
-    // ---------------------------------------------
-    // Add item
-    function addItem(name, price) {
-        for (let i = 0; i< cart.length; i += 1){
-            if (cart[i].name === name) {
-                cart[i].qty += 1
-                return
-            }
-        }
-        const item = { name, price, qty: 1 }
-        cart.push(item)
-    }
-
-    // ------------------------------------------------------
-    // Show Items
-    function showItems() {
-        const qty = getQty()
-        itemQty.innerHTML =  `You have ${qty} items in your cart`
-        
-        let itemStr = ''
-        for (let i = 0; i < cart.length; i+=1) {
-            // console.log(`- ${cart[i].name} $${cart[i].price} x ${cart[i].qty}`)
-    
-            // { name:'Apple'; price: 0.99; qty: 3}
-            const { name, price, qty } = cart[i]
-            itemStr += `<li> ${name} $${price} x ${qty} = $${price * qty} </li>`
-        }
-        itemList.innerHTML = itemStr
-        
-        const total = getTotal()
-        itemTotal.innerHTML= `Total in cart: $${total}`
-    }
-
-    // -----------------------------------------------------------
-    // Get Qty
-    function getQty() {
-        let qty = 0
-        for (let i = 0; i < cart.length; i+=1) {
-            qty += cart[i].qty
-        }
-        return qty
-    }
-
-
-    // -----------------------------------------------------------
-    // Get total
-    function getTotal() {
-        let total = 0
-        for (let i = 0; i < cart.length; i +=1) {
-            total += cart[i].price * cart[i].qty   
-        }
-        return total.toFixed(2)
-    }
-
-    // -------------------------------------------------------------
-    // Get Item
-    function removeItem(name, qty = 0) {
-        for (let i = 0; i < cart.length; i += 1) {
-            if (cart[i].name === name) {
-                if (qty > 0) {
-                    cart[i].qty -= qty
-                }
-                if (cart[i].qty < 1 || qty === 0) {
-                    cart.splice(i, 1)
-                }
-                return
-            }
-        }
-    }
-
-    // -------------------------------------------------------
-    addItem('Apple', 0.99)
-    addItem('Apple', 0.99)
-    addItem('Orange', 1.29)
-    addItem('Frisbee', 9.92)
-    showItems()
-    removeItem('Frisbee')
-    showItems()
-    removeItem('Apple', 1)
-    showItems()
-
 }
+
+const all_items_button = Array.from(document.querySelectorAll("button"))
+all_items_button.forEach(elt => elt.addEventListener('click', () => {
+    addItem(elt.getAttribute('id'), elt.getAttribute('data-price'))
+    showItems()
+}))
+console.log(all_items_button)
+
+const cart = []
+// ---------------------------------------------
+// Add item
+function addItem(name, price) {
+    for (let i = 0; i< cart.length; i += 1){
+        if (cart[i].name === name) {
+            cart[i].qty += 1
+            return
+        }
+    }
+    const item = { name, price, qty: 1 }
+    cart.push(item)
+}
+
+// ------------------------------------------------------
+// Show Items
+function showItems() {
+    const qty = getQty()
+    itemQty.innerHTML =  `You have ${qty} items in your cart`
+    
+    let itemStr = ''
+    for (let i = 0; i < cart.length; i+=1) {
+        // console.log(`- ${cart[i].name} $${cart[i].price} x ${cart[i].qty}`)
+
+        // { name:'Apple'; price: 0.99; qty: 3}
+        const { name, price, qty } = cart[i]
+        itemStr += `<li> ${name} $${price} x ${qty} = $${price * qty} </li>`
+    }
+    
+    itemList.innerHTML = itemStr
+    
+    const total = getTotal()
+    itemTotal.innerHTML= `Total in cart: $${total}`
+}
+
+// -----------------------------------------------------------
+// Get Qty
+function getQty() {
+    let qty = 0
+    for (let i = 0; i < cart.length; i+=1) {
+        qty += cart[i].qty
+    }
+    return qty
+}
+
+
+// -----------------------------------------------------------
+// Get total
+function getTotal() {
+    let total = 0
+    for (let i = 0; i < cart.length; i +=1) {
+        total += cart[i].price * cart[i].qty   
+    }
+    return total.toFixed(2)
+}
+
+// -------------------------------------------------------------
+// Get Item
+function removeItem(name, qty = 0) {
+    for (let i = 0; i < cart.length; i += 1) {
+        if (cart[i].name === name) {
+            if (qty > 0) {
+                cart[i].qty -= qty
+            }
+            if (cart[i].qty < 1 || qty === 0) {
+                cart.splice(i, 1)
+            }
+            return
+        }
+    }
+}
+
+// -------------------------------------------------------
+addItem('Apple', 0.99)
+addItem('Apple', 0.99)
+addItem('Orange', 1.29)
+addItem('Frisbee', 9.92)
+showItems()
+removeItem('Frisbee')
+showItems()
+removeItem('Apple', 1)
+showItems()
+
 
